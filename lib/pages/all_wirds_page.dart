3a24e42473a3a6dec84b8e666c03e42724fee7e3
Adnan Hussain <itzadnantech@gmail.com>
2022-10-03 21:56:31 +0500

    updated

--- lib/pages/all_wirds_page.dart
+++ lib/pages/all_wirds_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names, prefer_interpolation_to_compose_strings
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:wird_book/localization/language_constants.dart';
@@ -9,6 +11,8 @@ import 'package:wird_book/pages/setting_page.dart';
 
 import 'package:wird_book/config.dart' as config;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 
 class AllWirdsPage extends StatefulWidget {
   final String wird_cat_id;
@@ -38,6 +42,8 @@ class _AllWirdsPageState extends State<AllWirdsPage> {
   double _value = config.fontSize_min;
   double _prevScale = config.prevScale;
   double _scale = config.scale;
+  final controller = SwiperController();
+  double _currentValue = 10;
 
   @override
   void initState() {
@@ -51,6 +57,7 @@ class _AllWirdsPageState extends State<AllWirdsPage> {
             medium.wird_sub_cat_id == widget.wird_sub_cat_id &&
             medium.wird_cat_id == widget.wird_cat_id)
         .toList();
+    ;
   }
 
   void init_fontSize() async {
@@ -169,15 +176,11 @@ class _AllWirdsPageState extends State<AllWirdsPage> {
         appBar: AppBar(
           backgroundColor: Color(config.colorPrimary),
           centerTitle: true,
-          title: Text(getTranslated(
-              context,
-              "wird_sub_cat_id_" +
-                  widget.wird_cat_id +
-                  "_" +
-                  widget.wird_sub_cat_id)),
+          title: Text(getTranslated(context,
+              "wird_sub_cat_id_${widget.wird_cat_id}_${widget.wird_sub_cat_id}")),
           actions: <Widget>[
             IconButton(
-                icon: Icon(Icons.settings),
+                icon: const Icon(Icons.settings),
                 onPressed: () {
                   Navigator.of(context).push(
                       MaterialPageRoute(builder: (context) => SettingPage()));
@@ -185,13 +188,14 @@ class _AllWirdsPageState extends State<AllWirdsPage> {
           ],
         ),
         body: Padding(
-          padding: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 10),
+          padding:
+              const EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 10),
           child: Column(
             children: [
               Flexible(
                   flex: 1,
                   child: Container(
-                    padding: EdgeInsets.only(
+                    padding: const EdgeInsets.only(
                         top: 10, left: 80, right: 80, bottom: 0),
                     child: ValueListenableBuilder<ProgressBarState>(
                       valueListenable: progressNotifier,
@@ -201,7 +205,8 @@ class _AllWirdsPageState extends State<AllWirdsPage> {
                           buffered: value.buffered,
                           total: value.total,
                           onSeek: seek,
-                          baseBarColor: Color.fromARGB(255, 169, 170, 179),
+                          baseBarColor:
+                              const Color.fromARGB(255, 169, 170, 179),
                           progressBarColor: Color(config.colorPrimary),
                           thumbColor: Color(config.colorPrimary),
                         );
@@ -211,10 +216,11 @@ class _AllWirdsPageState extends State<AllWirdsPage> {
               Flexible(
                   flex: 2,
                   child: Container(
-                    padding: EdgeInsets.only(
-                        top: 0, left: 15, right: 15, bottom: 20),
+                    padding: const EdgeInsets.only(
+                        top: 0, left: 15, right: 15, bottom: 0),
                     child: ValueListenableBuilder<ButtonState>(
                       valueListenable: buttonNotifier,
+                      // ignore: missing_return
                       builder: (_, value, __) {
                         switch (value) {
                           case ButtonState.loading:
@@ -229,14 +235,14 @@ class _AllWirdsPageState extends State<AllWirdsPage> {
                           case ButtonState.paused:
                             return IconButton(
                               icon: const Icon(Icons.play_arrow),
-                              padding: EdgeInsets.all(2),
+                              padding: const EdgeInsets.all(2),
                               iconSize: 32.0,
                               onPressed: play,
                             );
                           case ButtonState.playing:
                             return IconButton(
                               icon: const Icon(Icons.pause),
-                              padding: EdgeInsets.all(2),
+                              padding: const EdgeInsets.all(2),
                               iconSize: 32.0,
                               onPressed: pause,
                             );
@@ -244,30 +250,47 @@ class _AllWirdsPageState extends State<AllWirdsPage> {
                       },
                     ),
                   )),
+              SizedBox(height: 30),
               Flexible(
-                  flex: 16,
+                  flex: 2,
                   child: Container(
-                    child: ListView.builder(
-                        itemCount: wirds.length,
-                        itemBuilder: (context, index) {
-                          final single_wird = wirds[index];
-                          String wird_translate = "wird_id_" +
-                              single_wird.wird_cat_id +
-                              "_" +
-                              single_wird.wird_sub_cat_id +
-                              "_" +
-                              single_wird.wird_id;
-                          String wird_count = getTranslated(context, 'wird') +
-                              '  ' +
-                              getTranslated(context, single_wird.wird_id);
-                          String Repetition = getTranslated(
-                                  context, 'repetition') +
-                              '  ' +
-                              getTranslated(context, single_wird.repetition);
-                          return WirdCards(single_wird, wird_translate,
-                              Repetition, wird_count, context);
-                        }),
+                    padding: const EdgeInsets.only(
+                        top: 0, left: 15, right: 15, bottom: 0),
+                    child: FAProgressBar(
+                      currentValue: _currentValue,
+                      size: 13,
+                      maxValue: 60,
+                      changeColorValue: 100,
+                      // changeProgressColor: Colors.pink,
+                      backgroundColor: Color.fromARGB(255, 169, 170, 179),
+                      progressColor: Color(config.colorPrimary),
+                      animatedDuration: const Duration(milliseconds: 300),
+                      direction: Axis.horizontal,
+                      verticalDirection: VerticalDirection.down,
+                      displayText: '%',
+                      formatValueFixed: 0,
+                    ),
                   )),
+              Flexible(
+                flex: 16,
+                child: Swiper(
+                  controller: SwiperController(),
+                  control: SwiperControl(
+                      padding: EdgeInsets.only(left: 70, right: 70, top: 215)),
+                  itemBuilder: (BuildContext context, int index) {
+                    final single_wird = wirds[index];
+                    String wird_translate =
+                        "wird_id_${single_wird.wird_cat_id}_${single_wird.wird_sub_cat_id}_${single_wird.wird_id}";
+                    String wird_count =
+                        '${getTranslated(context, 'wird')}  ${getTranslated(context, single_wird.wird_id)}';
+                    String Repetition =
+                        '${getTranslated(context, 'repetition')}  ${getTranslated(context, single_wird.repetition)}';
+                    return _buildListItem(single_wird, wird_translate,
+                        Repetition, wird_count, context);
+                  },
+                  itemCount: wirds.length,
+                ),
+              ),
             ],
           ),
         ),
@@ -275,38 +298,66 @@ class _AllWirdsPageState extends State<AllWirdsPage> {
     );
   }
 
-  @override
-  Widget WirdCards(
-          Wird single_wird, wird_translate, Repetition, wird_count, context) =>
+  Widget _buildListItem(Wird single_wird, wird_translate, Repetition,
+      wird_count, BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(12),
+      color: Colors.white,
+      elevation: 10,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 150,
+              child: ListTile(
+                contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
+                title: Text(
+                  getTranslated(context, wird_translate),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: fontSize(), fontWeight: FontWeight.w600),
+                ),
+                // subtitle: Text(
+                //   '\n' + Repetition,
+                //   // wird_count + '\n\n' + Repetition + '\n\n',
+                //   // getTranslated(context, "wird_" + single_wird.wird_id),
 
-      // width: MediaQuery.of(context).size.width * 0.8,
-      Card(
-        color: Colors.white,
-        elevation: 10,
-        child: Padding(
-            padding:
-                const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
-            child: ListTile(
-              contentPadding: EdgeInsets.symmetric(vertical: 8.0),
-              title: Text(
-                getTranslated(context, wird_translate),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: fontSize(), fontWeight: FontWeight.w600),
+                //   textAlign: TextAlign.center,
+                //   style: TextStyle(
+                //       fontSize: fontSize(),
+                //       color: Color(config.colorPrimary),
+                //       fontWeight: FontWeight.w500),
+                // ),
               ),
-              subtitle: Text(
-                '\n' + Repetition,
-                // wird_count + '\n\n' + Repetition + '\n\n',
-                // getTranslated(context, "wird_" + single_wird.wird_id),
-
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: fontSize(),
-                    color: Color(config.colorPrimary),
-                    fontWeight: FontWeight.w500),
-              ),
-            )),
-      );
+            ),
+            const Divider(),
+            const SizedBox(height: 100),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Flexible(
+                  flex: 2,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      controller.move(1);
+                    },
+                    child: Text(Repetition),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(150, 30),
+                      foregroundColor: Colors.white,
+                      backgroundColor: Color(config.colorPrimary),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(32.0),
+                          side: BorderSide(color: Color(config.colorPrimary))),
+                      padding: const EdgeInsets.only(
+                          top: 10, bottom: 10, right: 15, left: 15),
+                    ),
+                  )),
+            ])
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class ProgressBarState {
